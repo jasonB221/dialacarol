875eb92374d636cad00f675cdc2f7717655e64c8
Jason Bilas <jasonbilas@gmail.com>
2017-11-29 21:58:51 -0600

    Updated build script to run npm install

--- build.sh
+++ build.sh
@@ -23,6 +23,10 @@ cd ../build/
 cp -r ./* /var/www/dialacarol/public_html/
 #Deploy to cloud hosting
 firebase deploy --token "$DEPLOY_TOKEN" --project "$FIREBASE_PROJECT_ID" --only hosting
+
+#Handle function deployment
+cd ../functions/
+npm install
 firebase deploy --token "$DEPLOY_TOKEN" --project "$FIREBASE_PROJECT_ID" --only functions
 
 #Take care of backend variable replacement
