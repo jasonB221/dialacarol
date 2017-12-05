e550c99723d82b68cf7ac3b6c3f2d12c8b22f755
Jason Bilas <jasonbilas@gmail.com>
2017-12-04 21:22:01 -0600

    Begin switch to generalized year loading

--- web/js/init.js
+++ web/js/init.js
@@ -1,3 +1,7 @@
+//This variable holds the current year loaded by the maps
+var global_yearLoaded;
+var global_currentYear = (new Date()).getCurrentYear();
+
 //This is the callback function that google maps calls
 function initMap(){
     map = new google.maps.Map(document.getElementById("map"), { //This data is outdated, you should manually adjust the map on the screen
@@ -14,7 +18,7 @@ function initMap(){
         {imagePath: '/images/m', gridSize: 18});
 
     //This block of code requests all the previously recorded songs
-    var xmlHttp = new XMLHttpRequest();
+/*    var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
         if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
             //Executes when the code returns
@@ -22,7 +26,43 @@ function initMap(){
         }
     }
     xmlHttp.open("GET", "https://dialacarol.bilas.org/songs.json?t=" + (Date.now()/1000), true);
-    xmlHttp.send(null);
+    xmlHttp.send(null);*/
+    loadYear(global_currentYear);
+}
+
+function loadYear(year){
+    if(year == global_yearLoaded) return; //The map is already displaying the selected year
+    if(year == global_currentYear){
+        //loadCurrentYear();
+        alert("Year set to current year: " + year);
+        return;
+    }
+    else if(year > global_currentYear){
+        console.error("Year to set cannot be in the future");
+        return;
+    }
+
+    // Load a year in the past
+    global_yearLoaded = year;
+
+    if(!localStorage.getItem(year) == NULL){
+        // Year is not in local cache, fetch it from the remote server
+        var storageRef = firebase.storage().ref();
+        var ref = storageRef.child("songs/songs-" + year);
+        ref.getDownloadURL().then(function(url){
+            var xhr = new XMLHttpRequest();
+            xhr.responseType = "json";
+            xhr.onload = function(event){
+                localStorage.setItem(year, JSON.stringify(xhr.response));
+                addMultipleCalls(xhr.response["songs"]);
+            };
+            xhr.open("GET", url);
+            xhr.send();
+        }).catch(function(error){
+            console.error(error);
+        });
+    } //The response is already cached, instead of requesting again just use the cache
+    else addMultipleCalls(JSON.parse(localStorage.getItem(year))["songs"]);
 }
 
 //This function handles code for adding mass numbers of calls
