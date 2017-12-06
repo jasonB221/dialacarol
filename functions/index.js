d86e7bc5c2920c0ffa14b5e16dfc31b2d44daff2
Jason Bilas <jasonbilas@gmail.com>
2017-12-05 18:01:38 -0600

    Removed commented example code

--- functions/index.js
+++ functions/index.js
@@ -7,13 +7,6 @@ const admin = require('firebase-admin');
 
 admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
 /* This function registers a user to the current year's topic.
  * It is passed a query named token, and replies with a 200 status code.
  */
